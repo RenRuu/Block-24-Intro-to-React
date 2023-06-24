a2bfb7dcf5a5f2e3e16633a1bd9b33d902cbe5a5
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-06-23 21:58:22 -0700

    puppy data with useState completed

--- Workshop/puppy-pals/src/App.jsx
+++ Workshop/puppy-pals/src/App.jsx
@@ -1,15 +1,24 @@
+import {puppyList} from './data.js'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log("puppyList: ", puppyList); 
 
-  return (
-    <>
-      <div>
 
+  return ( 
+      <div className ="App">
+        { 
+            puppies.map((puppy) => {
+              return <p key={puppy.id}>{puppy.name}</p>;
+          })
+      }
       </div>
-
-    </>
-  )
+  );
 }
 
 export default App
+
+
+
